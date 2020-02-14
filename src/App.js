8127f8f30cd7cfb75748756f3d2fc246f068e990
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-02-14 14:21:13 +0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Tweet from './Tweet';
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+  // const [user, setUser] = useState({
+  //   name: 'Ferd',
+  //   surname: 'Yuska',
+  //   age: 25,
+  //   posts: ['mi first post', 'my very big status', 'salama kelle paca']
+  // });
+
+  const plussCount = () => {
+    setCount(count + 1);
+  };
+  const minusCount = () => {
+    setCount(count - 1);
+  };
+  const changeColor = () => {
+    setRed(!isRed);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className={isRed ? 'red' : 'blue'}>Change my color</h1>
+      <button onClick={changeColor}>Change color</button>
+      <button onClick={plussCount}>Pluss</button>
+      <h1>{count}</h1>
+      <button onClick={minusCount}>Minus</button>
     </div>
   );
 }
---
+++ src/Tweet.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+  
+function Tweet({name,message}) {
+  return (
+    <div className='tweet'>
+      
+      <h3>{name}</h3>
+      <p><strong>{message}</strong></p>
+      <h3>Number of likes</h3>
+    </div>
+  );
+}
+
+export default Tweet;
