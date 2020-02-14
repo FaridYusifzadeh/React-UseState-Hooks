02ae025e5dc08241c48492612a4c84a6806a0f21
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-02-14 14:40:28 +0400

    api date

--- src/App.js
+++ src/App.js
@@ -3,31 +3,40 @@ import './App.css';
 import Tweet from './Tweet';
 
 function App() {
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
+  // const [isRed, setRed] = useState(false);
+  // const [count, setCount] = useState(0);
   // const [user, setUser] = useState({
   //   name: 'Ferd',
   //   surname: 'Yuska',
   //   age: 25,
-  //   posts: ['mi first post', 'my very big status', 'salama kelle paca']
+  //   posts: ['mi first post', 'my very big status', 'test test test']
   // });
 
-  const plussCount = () => {
-    setCount(count + 1);
-  };
-  const minusCount = () => {
-    setCount(count - 1);
-  };
-  const changeColor = () => {
-    setRed(!isRed);
-  };
+  // const plussCount = () => {
+  //   setCount(count + 1);
+  // };
+  // const minusCount = () => {
+  //   setCount(count - 1);
+  // };
+  // const changeColor = () => {
+  //   setRed(!isRed);
+  // };
+
+  const [users, setUsers] = useState([
+    {name:'Farid', message:'Hello Iam Farid'},
+    {name:'Sebuhi',message:'Hello Iam Sebuhi'},
+    {name:'Tural',message:'Hello Iam Tural'}
+  ])
   return (
     <div className='App'>
-      <h1 className={isRed ? 'red' : 'blue'}>Change my color</h1>
+      {/* <h1 className={isRed ? 'red' : 'blue'}>Change my color</h1>
       <button onClick={changeColor}>Change color</button>
       <button onClick={plussCount}>Pluss</button>
       <h1>{count}</h1>
-      <button onClick={minusCount}>Minus</button>
+      <button onClick={minusCount}>Minus</button> */}
+      {users.map((data,index)=>(
+        <Tweet key={index} name={data.name} message={data.message} />
+      ))}
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-  
-function Tweet({name,message}) {
+function Tweet({ name, message }) {
   return (
     <div className='tweet'>
-      
       <h3>{name}</h3>
-      <p><strong>{message}</strong></p>
+      <p>
+        <strong>{message}</strong>
+      </p>
       <h3>Number of likes</h3>
     </div>
   );
